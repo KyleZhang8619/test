adf27bbe269900c9cfb90628823a5f47067d8394
KyleZhang8619 <agenting2009@126.com>
2014-08-03 15:39:18 +0800

    the first jekins project

---
+++ src/JavaLearn/traing01.java
@@ -0,0 +1,32 @@
+package JavaLearn;
+
+public class traing01 {
+	public void main(){
+	String s ;
+	String s1 = new String("abc");
+	
+	
+	System.out.println(s1);
+	
+
+
+
+
+
+	class kyle{};
+	kyle kk = new kyle();
+	
+	
+	class DataOnly{
+		int a;
+		double b;
+
+	}
+		
+
+	DataOnly data = new DataOnly();//创建对象
+	
+	
+	
+	}
+}
